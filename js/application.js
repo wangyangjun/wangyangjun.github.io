93719f8be1e09bca0c0f7f10b0fedb03af778407
Yangjun Wang <yangjun.wan@luxus.fi>
2015-09-30 21:50:28 +0300

    Test

---
+++ js/application.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//Start by defining the main module and adding the module dependencies
+angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+
+// Setting HTML5 Location Mode
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
+	function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+	}
+]);
+
+//Then define the init function for starting up the application
+angular.element(document).ready(function() {
+	//Fixing facebook bug with redirect
+	if (window.location.hash === '#_=_') window.location.hash = '#!';
+
+	//Then init the app
+	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+});
+
+// Use Application configuration module to register a new module
+ApplicationConfiguration.registerModule('app');
+
+angular.module('app').config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		// Redirect to home view when route not found
+		$urlRouterProvider.otherwise('/');
+
+		// Home state routing
+		$stateProvider.
+		state('home', {
+			url: '/',
+			templateUrl: 'home.html'
+		});
+	}
+]);
