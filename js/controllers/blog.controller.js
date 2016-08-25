7a96a24884fd4a52d0a99a35f4ad14d62e05ac02
Yangjun Wang <yangjun.wang@eniram.fi>
2016-08-25 16:06:48 +0300

    fix arrow function issue in safari

--- js/controllers/blog.controller.js
+++ js/controllers/blog.controller.js
@@ -18,7 +18,7 @@ angular.module('app').controller('BlogController', ['$scope', '$rootScope', '$el
 			var elements = [];
 			// first one is title
 			elements = $($element.find("[marked]")[0]).find("[id]").slice(1);
-			$scope.elementsWithId = elements.map((i, e) => { return {'id': e.id, 'text': e.innerText}});
+			$scope.elementsWithId = elements.map(function(i, e){ return {'id': e.id, 'text': e.innerText}});
 		});
 	}
 ]);
